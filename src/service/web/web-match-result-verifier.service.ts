2f1c59f3f137fdd92686bd177bfed48fcb8d6520
bterpinc <borut.terpinc@kalmia.si>
2024-07-15 10:45:48 +0200

    Added comment

--- src/service/web/web-match-result-verifier.service.ts
+++ src/service/web/web-match-result-verifier.service.ts
@@ -55,6 +55,38 @@ export class WEBMatchResultVerifierService {
         return this.verifyRequest(fixedRequest);
     }
 
+    /* Verifies the match results against the following parameters:
+    *   - date: match date (unix timestamp without hour - rounded down start of the day).
+    *   - sport: match sport (enum Sports).
+    *   - gender: for which gender is the match (0 = male, 1 = female, 2 = mixed).
+    *   - teams: match teams.
+    * 
+    * The response is expected to contain the following parameters:
+    *   - timestamp: Unix timestamp of the exact match beginning
+    *   - result: Possible return values are 1 = team 1 won, 2 = team 2 won, 3 = draw
+    * 
+    * The verification survives if:
+    * - the result is 1, 2 or 3.
+    * - the timestamp it the exact match beginning. This is calculated by rounding the timestamp to the star of the day and comparing it to the request date parameter.
+    * 
+    * 
+    *    enum Sports {
+    *        Basketball,
+    *        Basketball3x3,
+    *        Badminton,
+    *        BeachVolley,
+    *        FieldHockey,
+    *        Football,
+    *        Handball,
+    *        TableTennis,
+    *        Tennis,
+    *        Volleyball,
+    *        WaterPolo
+    *    }
+    * 
+    * 
+    */
+
     async verifyRequest(fixedRequest: MatchResult_Request): Promise<AttestationResponseDTO_MatchResult_Response> {
         //-$$$<start-verifyRequest> Start of custom code section. Do not change this comment.
 
@@ -135,8 +167,8 @@ export class WEBMatchResultVerifierService {
 
 /**
  * Gets the match result from the API. Each verifier should implement its own function to get the results from the API.
+ * Data must be transferred in such a way that it complies with the response structure.
  * 
- *
  * @param requestBody Body of the request
  * @returns  Body of the response
  */
